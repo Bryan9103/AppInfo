1156bb2c581a5894a2460992a203324e02a55428
B <bryanandersen03@gmail.com>
2023-11-12 05:48:49 +0800

    Initial Commit

---
+++ AppInfo/AppInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppInfoApp.swift
+//  AppInfo
+//
+//  Created by Bryan Andersen on 2023/11/12.
+//
+
+import SwiftUI
+
+@main
+struct AppInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
